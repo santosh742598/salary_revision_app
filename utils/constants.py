df9f2eb3146a4600b4ae82018ab907fe45741e91
santosh <santoshkumarsahu@gmail.com>
2025-05-30 00:42:04 +0530

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,23 @@
+# Creating the needed constants directly here for self-contained usage
+
+# PRC minimums for grade groups like E1, E2, etc.
+prc_minimums = {
+    "E1": 40000,
+    "E2": 50000,
+    "E3": 60000,
+    "E4": 70000,
+    "E5": 80000,
+    "E6": 90000,
+    "E7": 100000,
+    "E8": 110000,
+    "E9": 120000
+}
+
+# Month to number mapping
+month_order = {
+    "Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4,
+    "May": 5, "Jun": 6, "Jul": 7, "Aug": 8,
+    "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12
+}
+
+prc_minimums, month_order
